a94281a2f3faf84e865504b28121571151a8d3c1
max <max@101.lu>
2023-06-02 09:45:51 +0200

    fixed types

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,6 @@
 export type TCFunc = (...args: any[]) => any
 
 export type TCF = {
-  <FB>(fallback: FB): <F extends TCFunc>(fn: F) => ReturnType<F> | FB
-  <FB, F extends TCFunc>(fallback: FB, fn: F): ReturnType<F> | FB
+  <FB>(fallback: FB): <F extends TCFunc>(fn: F) => (...args: Parameters<F>) => ReturnType<F> | FB
+  <FB, F extends TCFunc>(fallback: FB, fn: F): (...args: Parameters<F>) => ReturnType<F> | FB
 }
